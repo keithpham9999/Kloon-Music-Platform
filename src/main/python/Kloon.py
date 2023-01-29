1279c11c76ae6c9d28dbd5c6688fc39d84926ac8
zjac147 <keith.pham.2020@live.rhul.ac.uk>
2023-01-29 17:18:31 +0000

    Add route for hompage html file to render the page

--- src/main/python/Kloon.py
+++ src/main/python/Kloon.py
@@ -7,7 +7,11 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
-
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    """Renders the home page
+    """
+    return render_template('home_page.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=6969)
