f03c43abc43d5b3892500f42514bcbfcdb1a6b6f
zjac147 <keith.pham.2020@live.rhul.ac.uk>
2023-03-25 03:21:20 +0000

    Add tracks

--- src/main/python/Database.py
+++ src/main/python/Database.py
@@ -41,6 +41,57 @@ class Database:
         self.create_menu_tables()
         self.create_order_tables()
 
+    def create_login_tables(self):
+        """Constructs the tables used for the login system
+        """
+        self.execute(
+            '''CREATE TABLE IF NOT EXISTS roles(
+            role_id SERIAL  PRIMARY KEY,
+            role_name       VARCHAR(16) NOT NULL, 
+            role_desc       VARCHAR(512) NOT NULL
+            )''')
+
+        self.execute(
+            '''CREATE TABLE IF NOT EXISTS staff(
+            staff_id SERIAL PRIMARY KEY,
+            first_name      VARCHAR(16) NOT NULL, 
+            last_name       VARCHAR(16) NOT NULL,
+            username        VARCHAR(16) UNIQUE NOT NULL,
+            password        VARCHAR(32) NOT NULL,
+            role_id         SERIAL NOT NULL
+            )''')
+        self.commit()
+
+    def create_music_tables(self):
+        """Constructs the tables used for storing music system
+        """
+
+        self.execute(
+            '''CREATE TABLE IF NOT EXISTS music(
+            track_id SERIAL PRIMARY KEY,
+            track_source     VARCHAR(16) NOT NULL,
+            track_name      VARCHAR(16) NOT NULL, 
+            artist_name       VARCHAR(16) NOT NULL,
+            genre        VARCHAR(16) NOT NULL
+            )''')
+        self.commit()
+
+    def fill_music(self):
+        """Fills the music table with the tracks
+        """
+        # This method was required for testing against a database, it will be removed once there is a database
+        # set in place
+        try:
+            self.execute("INSERT INTO music values(1, "music/4B x Junkie Kid - Love Is Dead.mp3", 'N/A')")
+            self.execute("INSERT INTO music values(2, 'Chef', 'N/A')")
+            self.execute("INSERT INTO music values(3, 'Manager', 'N/A')")
+            self.commit()
+        except:
+            pass
+
+    def music_reset(self):
+        self.create_music_tables()
+
     def close(self):
         """Terminates the connection to the database
         """
--- src/main/python/Kloon.py
+++ src/main/python/Kloon.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, flash
 from flask_socketio import SocketIO
 
+from Database import Database
 
 
 
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = "secret key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kloon.db'
 @app.route('/', methods=['POST', 'GET'])
 def index():
     """Renders the home page
@@ -14,4 +16,5 @@ def index():
     return render_template('home_page.html')
 
 if __name__ == "__main__":
+    Database.music_reset()
     app.run(debug=True, port=6969)
